ec4558ec0ed5b923354be499f86c196514950ef6
KENG!sBackward <qwwasszxx2345@gmail.com>
2025-03-29 11:24:44 +0700

    update user\[route]\route.ts

--- src/app/api/user/[route]/route.ts
+++ src/app/api/user/[route]/route.ts
@@ -1,12 +1,64 @@
+import { userSevice } from "@/app/lib/user";
+import { ResponseService } from "@/interface/responseService";
+import { UserData } from "@/interface/userInterface";
 import { NextRequest, NextResponse } from "next/server";
 
-export const PUT = async (
+export const POST = async (
   request: NextRequest,
   context: { params: { route: string } }
 ) => {
-  const id = context?.params?.route ?? "";
+  const route = context?.params?.route ?? "";
   const body = await request.json();
-  return NextResponse.json({ body, id }, { status: 200 });
+
+  if (route === "login") {
+    return await login(body);
+  } else if (route === "register") {
+    return await register(body);
+  }
+};
+
+export const login = async (data: any) => {
+  try {
+    const result = (await userSevice.login(data)) as UserData;
+
+    const response: ResponseService<UserData> = {
+      success: true,
+      message: "",
+      data: result,
+    };
+
+    return NextResponse.json(response);
+  } catch (error: any) {
+    const response: ResponseService<UserData> = {
+      success: false,
+      message: error,
+      data: null,
+    };
+
+    return NextResponse.json(response, { status: 401 });
+  }
+};
+
+export const register = async (data: any) => {
+  try {
+    const result = (await userSevice.register(data)) as UserData;
+
+    const response: ResponseService<UserData> = {
+      success: true,
+      message: "",
+      data: result,
+    };
+
+    return NextResponse.json(response);
+  } catch (error: any) {
+    const response: ResponseService<UserData> = {
+      success: false,
+      message: error,
+      data: null,
+    };
+
+    return NextResponse.json(response, { status: 401 });
+  }
 };
 
 export const DELETE = async (
